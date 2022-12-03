ab42b79fb1c7c9209fd995083feb2f97b992e62f
Admin <2762713521@qq.com>
2022-12-02 17:48:56 -0800

    x

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ import os;
 // how to split package name space
 // msvc can not detect module
 // windows driver detect module
+// rust and carbon test
+// c++ class test
+// c++ namespace test
 
 int main() {
     hello();
